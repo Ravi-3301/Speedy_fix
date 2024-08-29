62d84fba22716ca1db3001d449bb02939ee0099c
Vaishnavi-1910 <vaisnavi1910@gmail.com>
2024-08-29 22:08:20 +0530

    hello

--- app/src/main/java/com/example/speedyfix/PhoneInputFragment.java
+++ app/src/main/java/com/example/speedyfix/PhoneInputFragment.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.FirebaseException;
@@ -24,7 +25,7 @@ public class PhoneInputFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_phone_input, container, false);
-
+        Button text=
         Button btnNext = view.findViewById(R.id.btnNext);
         EditText phoneNumber = view.findViewById(R.id.etPhoneNumber);
         btnNext.setOnClickListener(v -> {
