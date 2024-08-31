97e04f8ce7e58b9822b9b2d8499f9bec02b6d308
Vaishnavi-1910 <vaisnavi1910@gmail.com>
2024-08-31 11:53:18 +0530

    error commit
    error commit

--- app/src/main/java/com/example/speedyfix/PhoneInputFragment.java
+++ app/src/main/java/com/example/speedyfix/PhoneInputFragment.java
@@ -25,7 +25,7 @@ public class PhoneInputFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_phone_input, container, false);
-        Button text=
+        
         Button btnNext = view.findViewById(R.id.btnNext);
         EditText phoneNumber = view.findViewById(R.id.etPhoneNumber);
         btnNext.setOnClickListener(v -> {
